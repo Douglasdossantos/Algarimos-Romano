14eb1b7edad5d632dceb6934fa7ced42a97a8d7e
Douglas costa <douglasalgustocosta@gmail.com>
2020-03-13 03:58:33 -0300

    Add project files.

---
+++ Algarimos Romano/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Algarimos_Romano
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Algarimos Romano/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Algarimos_Romano.MvcApplication" Language="C#" %>
